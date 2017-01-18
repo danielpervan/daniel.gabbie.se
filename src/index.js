e734bdb87b6910c677f3326254e6eecb1366311f
Daniel Pervan <danielpervan@Daniels-MacBook-Pro.local>
2017-01-18 14:03:52 +0100

    Refaktorisering av struktur

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from './routes';
 import './index.css';
+import { browserHistory } from 'react-router'
+
+window.$ = window.jQuery = require("jquery")
+require('foundation-sites/dist/js/foundation.min.js')
+require('foundation-sites/dist/css/foundation.min.css')
 
 ReactDOM.render(
-  <App />,
+  <Routes history={browserHistory}/>,
   document.getElementById('root')
 );
