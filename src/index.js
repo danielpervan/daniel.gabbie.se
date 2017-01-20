abe1d2d2a76b92c8043d7c91ae7e023ede754f56
Daniel Pervan <danielpervan@Daniels-MacBook-Pro.local>
2017-01-20 22:07:48 +0100

    Routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
-import './index.css';
+import './index.scss';
 import { browserHistory } from 'react-router'
 
 window.$ = window.jQuery = require("jquery")
-require('foundation-sites/dist/js/foundation.min.js')
-require('foundation-sites/dist/css/foundation.min.css')
+require('foundation-sites')
 
 ReactDOM.render(
   <Routes history={browserHistory}/>,
