1789e1c147610c7f971f59d543a8c004dc50c977
Daniel Pervan <danielpervan@Daniels-MacBook-Pro.local>
2017-01-20 22:07:36 +0100

    Implementerat förstasidan

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PageHeader from '../PageHeader'
+import Flare from '../Flare'
+import SaveDateCard from '../SaveDateCard'
+import $ from 'jquery'
+
 
 class App extends Component {
+  constructor(props) {
+    super()
+    this.flares = []
+    generateFlares(this.flares)
+    $(window).resize(() => {
+      this.flares = []
+      generateFlares(this.flares)
+    })
+  }
+
+
+
+  componentDidMount() {
+    $('.flares').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', () => {
+      $(document).on('mousemove', (ev) => {
+        animateFlares(ev.pageX, ev.pageY)
+      })
+      animateFlares(Math.random()*$(document).width(), Math.random()*$(document).height())
+    })
+
+    $('.flares').css({
+      animationName: 'initFlares'
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="flares">{this.flares}</div>
+        <PageHeader/>
+        {true && <SaveDateCard/>}
       </div>
     );
   }
 }
 
+function generateFlares(storage) {
+  for (var i=0; i<100; i++) {
+      storage.push(<Flare key={i}/>)
+  }
+  
+}
+
+function animateFlares(x,y) {
+  const perspective = {
+    x: 100*x/$(document).width(),
+    y: 100*y/$(document).height()
+  }
+  $(".flares").css({
+    perspectiveOrigin: perspective.x + 'px ' + perspective.y + 'px'
+  })
+}
 export default App;
