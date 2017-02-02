bfc32fbebd77f75dc5ea86404d097dff6b56ce71
Daniel Pervan <danielpervan@Daniels-MacBook-Pro.local>
2017-02-03 00:38:01 +0100

    Fixat mailto-länk

--- src/components/GistPage/index.js
+++ src/components/GistPage/index.js
@@ -14,7 +14,7 @@ class GistPage extends Component {
       		<p>There will be a few different arrangement for different guests, but most of you will likely be staying at <a href="http://www.pervanovoapartments.com/default-en.html">Pervanovo Apartments</a>.</p>
       		<h2>Prolonging my stay</h2>
       		<p>While accomodation will be free during the wedding, some of you might be thinking of seize the opportunity to take a well-deserved vacation before or after the event.</p>
-      		<p>If you want to prolonge your stay, the apartments are available for rent. Contact Pervanovo at <a href="info@pervanovo.hr">info@pervanovo.hr</a>. Prices are available on the <a href="http://www.pervanovoapartments.com/default-en.html">website</a>.</p>
+      		<p>If you want to prolonge your stay, the apartments are available for rent. Contact Pervanovo at <a href="mailto:info@pervanovo.hr">info@pervanovo.hr</a>. Prices are available on the <a href="http://www.pervanovoapartments.com/default-en.html">website</a>.</p>
       	</TextBox>
         <SaveDateCard />
       </App>
